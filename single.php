c32f059015ce9083d5f22616ec9696678031b082
Putra Roeung <putra.roeung@gmail.com>
2017-10-05 15:18:42 -0400

    Moved content templates into template-parts/content/

--- single.php
+++ single.php
@@ -12,20 +12,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if (get_post_type()): ?>
+					<?php get_template_part( 'template-parts/content/content-', get_post_type( get_the_ID()) ); ?>
+				<?php else: ?>
+					<?php get_template_part( 'template-parts/content/content-', get_post_format() ); ?>
+				<?php endif; ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+				<?php the_post_navigation(); ?>
+				
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
